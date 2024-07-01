0b1152a876663300bf00b082a860419c7919b2e3
Paula Lopez <mplg070605@gmail.com>
2024-06-30 18:41:43 -0600

    Update mysql_connection.py

--- connection/mysql_connection.py
+++ connection/mysql_connection.py
@@ -1,5 +1,6 @@
 # Description: This file is used to connect to the MySQL database.
 # Please fill in the blanks with the correct credentials to connect to your database.
+# Hardcoding credentials is not recommended, but for the sake of simplicity, I have done so.
 def db_connection(app):
     app.config['MYSQL_HOST'] = ''
     app.config['MYSQL_USER'] = ''
