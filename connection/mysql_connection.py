566812cfe88d43858052ebe0f5f28ce82f61a6d1
garcia-lopez <mplg070605@gmail.com>
2024-07-03 19:40:04 -0600

    feat: Added new background animation

--- connection/mysql_connection.py
+++ connection/mysql_connection.py
@@ -2,8 +2,8 @@
 # Please fill in the blanks with the correct credentials to connect to your database.
 # Hardcoding credentials is not recommended, but for the sake of simplicity, I have done so.
 def db_connection(app):
-    app.config['MYSQL_HOST'] = ''
-    app.config['MYSQL_USER'] = ''
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = ''
-    return app
+  app.config['MYSQL_HOST'] = 'localhost'
+  app.config['MYSQL_USER'] = 'root'
+  app.config['MYSQL_PASSWORD'] = 'minionjc78#'
+  app.config['MYSQL_DB'] = 'login_system'
+  return app
